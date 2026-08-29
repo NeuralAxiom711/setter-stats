d5d2850f023e316a795b353813c8296f9eb92d3c
NeuralAxiom711 <281187923+NeuralAxiom711@users.noreply.github.com>
2026-08-29 00:17:10 -0600

    fix: soften and reposition Lauren watermark

--- Web/sw.js
+++ Web/sw.js
@@ -1,3 +1,3 @@
-const CACHE='setter-stats-v32';
+const CACHE='setter-stats-v33';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./volleyball-background.png']))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
